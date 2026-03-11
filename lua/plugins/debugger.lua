c237c83459b0ee6e31e2634be5b29ee63f8c6ae2
yayaduckd <114066508+yayaduckd@users.noreply.github.com>
2026-03-11 14:35:47 +0100

    idk

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,188 +1,112 @@
 return {
-	"mfussenegger/nvim-dap",
-	event = "VeryLazy",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"jay-babu/mason-nvim-dap.nvim",
-		"theHamsta/nvim-dap-virtual-text",
-	},
-	keys = {
-		{
-			"<leader>d",
-			group = "Debugger",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>dt",
-			function()
-				require("dap").toggle_breakpoint()
-			end,
-			desc = "Toggle Breakpoint",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>dc",
-			function()
-				require("dap").continue()
-			end,
-			desc = "Continue",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>di",
-			function()
-				require("dap").step_into()
-			end,
-			desc = "Step Into",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>do",
-			function()
-				require("dap").step_over()
-			end,
-			desc = "Step Over",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>du",
-			function()
-				require("dap").step_out()
-			end,
-			desc = "Step Out",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>dr",
-			function()
-				require("dap").repl.open()
-			end,
-			desc = "Open REPL",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>dl",
-			function()
-				require("dap").run_last()
-			end,
-			desc = "Run Last",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>dq",
-			function()
-				require("dap").terminate()
-				require("dapui").close()
-				require("nvim-dap-virtual-text").toggle()
-			end,
-			desc = "Terminate",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>db",
-			function()
-				require("dap").list_breakpoints()
-			end,
-			desc = "List Breakpoints",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>de",
-			function()
-				require("dap").set_exception_breakpoints({ "all" })
-			end,
-			desc = "Set Exception Breakpoints",
-			nowait = true,
-			remap = false,
-		},
-	},
-	config = function()
-		local mason_dap = require("mason-nvim-dap")
-		local dap = require("dap")
-		local ui = require("dapui")
-		local dap_virtual_text = require("nvim-dap-virtual-text")
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "jay-babu/mason-nvim-dap.nvim",
+        "theHamsta/nvim-dap-virtual-text",
+    },
+    keys = {
+        { "<leader>d",  group = "Debugger",                                nowait = true,              remap = false },
+        { "<leader>dt", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint", nowait = true, remap = false },
+        { "<leader>dc", function() require("dap").continue() end,          desc = "Continue",          nowait = true, remap = false },
+        { "<leader>di", function() require("dap").step_into() end,         desc = "Step Into",         nowait = true, remap = false },
+        { "<leader>do", function() require("dap").step_over() end,         desc = "Step Over",         nowait = true, remap = false },
+        { "<leader>du", function() require("dap").step_out() end,          desc = "Step Out",          nowait = true, remap = false },
+        { "<leader>dr", function() require("dap").repl.open() end,         desc = "Open REPL",         nowait = true, remap = false },
+        { "<leader>dl", function() require("dap").run_last() end,          desc = "Run Last",          nowait = true, remap = false },
+        {
+            "<leader>dq",
+            function()
+                require("dap").terminate()
+                require("dapui").close()
+                require("nvim-dap-virtual-text").toggle()
+            end,
+            desc = "Terminate",
+            nowait = true,
+            remap = false
+        },
+        { "<leader>db", function() require("dap").list_breakpoints() end,                   desc = "List Breakpoints",          nowait = true, remap = false },
+        { "<leader>de", function() require("dap").set_exception_breakpoints({ "all" }) end, desc = "Set Exception Breakpoints", nowait = true, remap = false },
+    },
+    config = function()
+        local mason_dap = require("mason-nvim-dap")
+        local dap = require("dap")
+        local ui = require("dapui")
+        local dap_virtual_text = require("nvim-dap-virtual-text")
 
-		-- Dap Virtual Text
-		dap_virtual_text.setup()
+        -- Dap Virtual Text
+        dap_virtual_text.setup()
 
-		mason_dap.setup({
-			ensure_installed = { "cppdbg", "codelldb" },
-			automatic_installation = true,
-			handlers = {
-				function(config)
-					require("mason-nvim-dap").default_setup(config)
-				end,
-			},
-		})
+        mason_dap.setup({
+            ensure_installed = { "cppdbg", "codelldb", "lldb" },
+            automatic_installation = true,
+            handlers = {
+                function(config)
+                    require("mason-nvim-dap").default_setup(config)
+                end,
+            },
+        })
 
-		-- Configurations
-		dap.configurations = {
-			c = {
-				{
-					name = "Launch file",
-					type = "cppdbg",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					cwd = "${workspaceFolder}",
-					stopAtEntry = false,
-					MIMode = "lldb",
-				},
-				{
-					name = "Attach to lldbserver :1234",
-					type = "cppdbg",
-					request = "launch",
-					MIMode = "lldb",
-					miDebuggerServerAddress = "localhost:1234",
-					miDebuggerPath = "/usr/bin/lldb",
-					cwd = "${workspaceFolder}",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-				},
-			},
-			zig = {
-				{
-					name = "Launch",
-					type = "codelldb",
-					request = "launch",
-					program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-					args = {},
-				},
-			},
-		}
+        -- Configurations
+        local lldbinit_cmd = "command source " .. vim.fn.expand("~/.lldbinit")
 
-		-- Dap UI
+        dap.configurations = {
+            c = {
+                {
+                    name = "Launch file (codelldb)",
+                    type = "codelldb", -- Switched to codelldb
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                    end,
+                    cwd = "${workspaceFolder}",
+                    stopOnEntry = false,
+                    initCommands = { lldbinit_cmd },
+                },
+                {
+                    name = "Attach to lldbserver :1234 (codelldb)",
+                    type = "codelldb",
+                    request = "attach",
+                    target = "localhost:1234",
+                    cwd = "${workspaceFolder}",
+                    program = function()
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                    end,
+                    initCommands = { lldbinit_cmd },
+                },
+            },
+            zig = {
+                {
+                    name = "Launch (codelldb)",
+                    type = "codelldb", -- Switched to codelldb
+                    request = "launch",
+                    program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
+                    cwd = "${workspaceFolder}",
+                    stopOnEntry = false,
+                    args = { "" },
+                    initCommands = { lldbinit_cmd },
+                },
+            },
+        }
 
-		ui.setup()
+        -- Dap UI
+        ui.setup()
 
-		vim.fn.sign_define("DapBreakpoint", { text = "🐞" })
+        vim.fn.sign_define("DapBreakpoint", { text = "🐞" })
 
-		dap.listeners.before.attach.dapui_config = function()
-			ui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			ui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			ui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			ui.close()
-		end
-	end,
+        dap.listeners.before.attach.dapui_config = function()
+            ui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            ui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            ui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            ui.close()
+        end
+    end,
 }
